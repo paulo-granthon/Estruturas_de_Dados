8cc930294db7f4c84a043de4d83b1110b37a7850
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-08-21 20:28:48 -0300

    add: 3_rec_print

---
+++ src/3_rec_print.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+static void print_recursive (int v[], int n) {
+    if (n == 0) {
+        printf("\n");
+        return;
+    }
+    print_recursive(v, n-1);
+    printf("\nnumber [%d]: %d", n, v[n-1]);
+}
+
+
+int main () {
+    print_recursive((int[]){1, 2, 3}, 3);
+    print_recursive((int[]){9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, 10);
+    print_recursive((int[]){82639, 20378, 1269, 95871, 2, 192}, 6);
+}
