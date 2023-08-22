0681e42a6731d36bf30ac224d381f1ed2db02842
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-08-21 21:48:51 -0300

    chore: update run.sh input handling

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ input_num="$1"
 if ! [[ "$input_num" =~ ^[0-9]+$ ]]; then
   echo "Invalid input. Please provide a number between 1 and $num_files."
 else
-  if ((input_num >= 1 && input_num <= num_files)); then
+  if ((input_num >= 0 && input_num <= num_files)); then
     file_to_compile=$(find src/ -maxdepth 1 -type f -name "${input_num}_*.c")
     file_name=$(basename "$file_to_compile" .c)
     
