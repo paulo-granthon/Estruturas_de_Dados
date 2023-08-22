868fbf8d370d98c29a6cfe1f94fd1ff7dd2d824e
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-08-21 21:02:10 -0300

    feat: better run.sh input handling

--- run.sh
+++ run.sh
@@ -1,5 +1,26 @@
-if [ "-d compiled/\"$1\"" ]; then rm compiled/"$1"; fi
-if [ ! -d "compiled" ]; then mkdir compiled; fi
-gcc -g src/"$1".c -o compiled/"$1"
-chmod u+x ./compiled/"$1"
-./compiled/"$1"
+#!/bin/bash
+
+num_files=$(find src/ -maxdepth 1 -type f -name '[0-9]*_*.c' | wc -l)
+input_num="$1"
+
+if ! [[ "$input_num" =~ ^[0-9]+$ ]]; then
+  echo "Invalid input. Please provide a number between 1 and $num_files."
+else
+  if ((input_num >= 1 && input_num <= num_files)); then
+    file_to_compile=$(find src/ -maxdepth 1 -type f -name "${input_num}_*.c")
+    file_name=$(basename "$file_to_compile" .c)
+    
+    if [ ! -d "compiled" ]; then
+      mkdir compiled
+    fi
+    
+    gcc -g "$file_to_compile" -o "compiled/$file_name"
+    chmod u+x "compiled/$file_name"
+    "./compiled/$file_name"
+  else
+    echo "Invalid number. Please provide a number between 1 and $num_files."
+    echo "Available files:"
+    find src/ -maxdepth 1 -type f -name '[0-9]*_*.c' -exec basename {} \;
+  fi
+fi
+
