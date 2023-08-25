19b8c228f7e38e8fc66035d37096165a8bd74fc4
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-08-24 21:34:02 -0300

    chore: update 3_rec_print function name

--- src/3_rec_print.c
+++ src/3_rec_print.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-static void print_recursive (int v[], int n) {
-    if (n > 1) print_recursive(v, n-1);
+static void print_recursive_array (int v[], int n) {
+    if (n > 1) print_recursive_array(v, n-1);
     printf("number [%d]: %d\n", n, v[n-1]);
 }
 
 
 int main () {
-    print_recursive((int[]){1, 2, 3}, 3);
-    print_recursive((int[]){9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, 10);
-    print_recursive((int[]){82639, 20378, 1269, 95871, 2, 192}, 6);
+    print_recursive_array((int[]){1, 2, 3}, 3);
+    print_recursive_array((int[]){9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, 10);
+    print_recursive_array((int[]){82639, 20378, 1269, 95871, 2, 192}, 6);
 }
