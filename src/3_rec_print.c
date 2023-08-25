fa35b576069bd0cce98c057e1a39d21f4acabb3f
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-08-24 22:49:28 -0300

    add: print_recursive int version

--- src/3_rec_print.c
+++ src/3_rec_print.c
@@ -5,9 +5,18 @@ static void print_recursive_array (int v[], int n) {
     printf("number [%d]: %d\n", n, v[n-1]);
 }
 
+static void print_recursive (int n) {
+    if (n > 1) print_recursive(n - 1);
+    printf("%d\n", n);
+}
+
 
 int main () {
     print_recursive_array((int[]){1, 2, 3}, 3);
     print_recursive_array((int[]){9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, 10);
     print_recursive_array((int[]){82639, 20378, 1269, 95871, 2, 192}, 6);
+
+    print_recursive(5);
+    print_recursive(11);
+    print_recursive(7);
 }
