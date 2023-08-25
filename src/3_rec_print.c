e97029c0c43f4a758527d6003df1e908ec5069b5
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-08-25 00:19:42 -0300

    chore: update 3_rec_print.c output format

--- src/3_rec_print.c
+++ src/3_rec_print.c
@@ -10,13 +10,26 @@ static void print_recursive (int n) {
     printf("%d\n", n);
 }
 
+static void print_and_run_array (int v[], int n) {
+    printf("testing recursive print for array [");
+    for (int i = 0; i < n - 1; i++) {
+        printf("%d, ", v[i]);
+    }
+    printf("%d]\n", v[n - 1]);
+    print_recursive_array(v, n);
+} 
+
+static void print_and_run_integer (int n) {
+    printf("testing recursive print for integer %d\n", n);
+    print_recursive(n);
+} 
 
 int main () {
-    print_recursive_array((int[]){1, 2, 3}, 3);
-    print_recursive_array((int[]){9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, 10);
-    print_recursive_array((int[]){82639, 20378, 1269, 95871, 2, 192}, 6);
+    print_and_run_array((int[]){1, 2, 3}, 3);
+    print_and_run_array((int[]){9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, 10);
+    print_and_run_array((int[]){82639, 20378, 1269, 95871, 2, 192}, 6);
 
-    print_recursive(5);
-    print_recursive(11);
-    print_recursive(7);
+    print_and_run_integer(5);
+    print_and_run_integer(11);
+    print_and_run_integer(7);
 }
