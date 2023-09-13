a9b5dd0d58c5a72718821bc632945f2a2e93b72e
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-09-12 21:13:59 -0300

    add (operations): `OPERATION_CONTAINS`

--- src/utils/operation_codes.c
+++ src/utils/operation_codes.c
@@ -4,7 +4,8 @@
 char* operation_to_string (int operation) {
     switch (operation) {
         case OPERATION_INSERT: return "OPERATION_INSERT";
-        case OPERATION_REMOVE: return "OPERATION_REMOVE_AT:";
+        case OPERATION_REMOVE: return "OPERATION_REMOVE:";
+        case OPERATION_CONTAINS: return "OPERATION_CONTAINS:";
         default: {
             static char buf[32];
             snprintf(buf, sizeof(buf), "UNKNOWN_OPERATION: %d", operation);
--- src/utils/operation_codes.h
+++ src/utils/operation_codes.h
@@ -1,4 +1,5 @@
 #define OPERATION_INSERT 0
 #define OPERATION_REMOVE 1
+#define OPERATION_CONTAINS 2
 
 char* operation_to_string (int operation);
