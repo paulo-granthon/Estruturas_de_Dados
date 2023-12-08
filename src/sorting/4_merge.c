5349c4c3d2648e375624808dae97409decc337c7
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-12-07 22:31:56 -0300

    chore: split combine logic into dedicated function in `sort_merge`

--- src/sorting/4_merge.c
+++ src/sorting/4_merge.c
@@ -5,49 +5,67 @@
 #include <stdio.h>
 #include "../utils/array_utils.h"
 
-void fake_indent(int indent) {
-    for (int i = 0; i < indent; i++) printf("| ");
+// void fake_indent(int indent) {
+//     for (int i = 0; i < indent; i++) printf("| ");
+// }
+
+void sort_merge_combine (int* array, int target_start, int target_end, int indent) {
+    int half = (target_end - target_start) / 2;
+    int i = target_start;
+    int j = target_start + half;
+    int temp = 0;
+
+    // fake_indent(indent); printf("combining | target_start: %d | target_end: %d\n", target_start, target_end);
+
+    while (i < target_start + half && j < target_end) {
+
+        // printf("i: [%d]%02d | j: [%d]%02d |=> ", i, array[i], j, array[j]);
+
+        if (array[j] >= array[i]) {
+            printf("SKIP...\n");
+
+            j++;
+            continue;
+        }
+
+        // printf("!SWAP!\n");
+
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+        // printf("i: [%d]%02d | j: [%d]%02d\n", i, array[i], j, array[j]);
+
+        // fake_indent(indent); array_print_slice(array, target_start, target_end);
+        i++;
+    }
 }
 
 void sort_merge (int* array, int target_start, int target_end, int indent) {
     int half = (target_end - target_start) / 2;
-    fake_indent(indent); printf("sort_merge | ts: %d | te: %d | half: %d\n", target_start, target_end, half);
-    fake_indent(indent); printf("input: ");
-    array_print_slice(array, target_start, target_end);
+    // fake_indent(indent); printf("sort_merge | ts: %d | te: %d | half: %d\n", target_start, target_end, half);
+    // fake_indent(indent); printf("input: "); array_print_slice(array, target_start, target_end);
+
     if (target_end - target_start > 2) {
         sort_merge(array, target_start, target_start + half, indent + 1);
         sort_merge(array, target_start + half, target_end, indent + 1);
     }
-    int i = target_start;
-    int j = half;
-    int temp = 0;
-    while (i < half && j < target_end) {
-        // printf( "s: %d | e: %d | i: [%d]%02d >=? j: [%d]%02d || ", target_start, target_end, i, array[i], j, array[j]);
-        if (array[j] >= array[i]) {
-            // printf("SKIP...\n");
-            j++;
-            continue;
-        }
-        // printf("!SWAP!\n");
-        temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-        i++;
-    }
-    fake_indent(indent); printf("output: ");
-    array_print_slice(array, target_start, target_end);
+    sort_merge_combine(array, target_start, target_end, indent);
+    // fake_indent(indent); printf("output: "); array_print_slice(array, target_start, target_end);
 }
 
 int main () {
 
-    int* array = array_create(10);
-    int* copy = array_copy(array, 10);
-    array_print(array, 10);
+    int len = 3;
+    int* array = array_create_desc(len);
+    int* copy = array_copy(array, len);
 
-    sort_merge(array, 0, 10, 0);
+    printf("\nSTART:\n");
+    array_print(array, len);
+    printf("\n");
+
+    sort_merge(array, 0, len, 0);
 
     printf("\nRESULT:\n");
-    array_print(copy, 10);
-    printf("   __\n  |  |\n  |  |\n _|  |_\n \\    /\n  \\  /\n   \\/\n");
-    array_print(array, 10);
+    array_print(array, len);
 }
